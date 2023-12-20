65b60873a0c0c420c5f9a36e1a802d36c0ffc4e6
M.Goekche AYDIN <63980772+Gokay2705@users.noreply.github.com>
2023-12-20 11:48:32 +0100

    style: :art: added: js file
    for navigation menu we added nav.js file

---
+++ nav.js
@@ -0,0 +1,21 @@
+const navShow = () => {
+    const menu = document.querySelector('.menu');
+    const nav = document.querySelector('ul');
+    const links = document.querySelectorAll('ul li');
+
+
+    menu.addEventListener('click',()=>{
+        nav.classList.toggle('nav-active');
+        links.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            }
+            else{
+                link.style.animation = `linksFade .5s ease forwards ${index / 5 + 0.2}s`;
+            }
+        });
+        menu.classList.toggle('close');
+    });
+};
+
+navShow();
