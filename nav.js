a26674d67bf01a9bd085f20b465cf4ac9b5b0e6e
M.Goekche AYDIN <63980772+Gokay2705@users.noreply.github.com>
2024-01-19 11:28:26 +0100

    fix: :zap: responsive menu

--- nav.js
+++ nav.js
@@ -1,21 +1,20 @@
 const navShow = () => {
-    const menu = document.querySelector('.menu');
-    const nav = document.querySelector('ul');
-    const links = document.querySelectorAll('ul li');
-
-
-    menu.addEventListener('click',()=>{
-        nav.classList.toggle('nav-active');
-        links.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            }
-            else{
-                link.style.animation = `linksFade .5s ease forwards ${index / 5 + 0.2}s`;
-            }
-        });
-        menu.classList.toggle('close');
+  const menu = document.querySelector(".menu");
+  const nav = document.querySelector("ul");
+  const links = document.querySelectorAll("ul li");
+  menu.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+    links.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `linksFade .5s ease forwards ${
+          index / 5 + 0.2
+        }s`;
+      }
     });
+    menu.classList.toggle("close");
+  });
 };
 
 navShow();
